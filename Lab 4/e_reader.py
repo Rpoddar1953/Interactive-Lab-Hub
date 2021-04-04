14ea88c6a4a003ed6993f500fd4bb5180dee7b9b
Ritika Poddar <rp477@cornell.edu>
2021-04-04 16:55:04 -0400

    updates

---
+++ Lab 4/e_reader.py
@@ -0,0 +1,38 @@
+import time
+import board
+import busio
+import subprocess
+import adafruit_mpr121
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
+mpr121 = adafruit_mpr121.MPR121(i2c)
+
+def handle_speak(val):
+    subprocess.run(["sh","GoogleTTS_demo.sh",val])
+
+
+while True:
+    for i in range(12):
+        touched = mpr121.touched_pins
+
+        if touched[1]:
+            print("1 touched")
+            #handle_speak("The definition of ambiguous is unclear or vague in meaning")
+
+        if touched[4]:
+            print ("4 touched")
+            #handle_speak(" this is the current sentence")
+        
+        if touched[10]:
+            print("10 touched")
+
+        if touched[6]:
+            print("6 touched")
+
+        if touched[1] and touched[10]:
+            handle_speak("touched a word")
+
+            
+    time.sleep(0.25)  # Small delay to keep from spamming output messages.
+
